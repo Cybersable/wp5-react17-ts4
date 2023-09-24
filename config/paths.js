57741e37dd1f5fdfc310d403d11a3820a3544771
e.zheltukhin <e.zheltukhin@kt-team.de>
2023-09-25 00:46:28 +0500

    Initial commit, first app template configuration

---
+++ config/paths.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const root = path.resolve(__dirname, '..')
+
+module.exports = {
+  entryPoint: path.resolve(root, 'src', 'index.tsx'),
+  build: path.resolve(root, 'build'),
+  src: path.resolve(root, 'src'),
+  public: path.resolve(root, 'src/app/public'),
+  indexHtml: path.resolve(root, 'src/app/public/index.html'),
+}
---
+++ config/webpack.common.js
@@ -0,0 +1,57 @@
+const paths = require('./paths')
+const { EsbuildPlugin } = require('esbuild-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: paths.entryPoint,
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: paths.build,
+    clean: true,
+    filename: '[name].[contenthash].js',
+    // assetModuleFilename: 'assets/[name][ext]',
+  },
+  module: {
+    rules: [
+      // Use esbuild to compile JavaScript & TypeScript
+      {
+        // Match `.js`, `.jsx`, `.ts` or `.tsx` files
+        test: /\.[jt]sx?$/,
+        loader: 'esbuild-loader',
+        options: {
+          target: 'es2015',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new EsbuildPlugin({
+        target: 'es2015',
+        css: true, // Apply minification to CSS assets
+      }),
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: paths.indexHtml,
+    }),
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['**/*.map'],
+      protectWebpackAssets: false,
+    }),
+    new CopyPlugin({
+      patterns: [
+        // { from: `${paths.public}/*.(ico|png|svg|json)`, to: '[name][ext]' },
+      ],
+    }),
+  ],
+}
